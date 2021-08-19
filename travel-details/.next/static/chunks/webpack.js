45e98ee8a4498f9a7b8bf9329f8ca3dd7244820a
Tyler Ma <tylermavan@gmail.com>
2021-08-19 05:57:56 +0000

    Add posts.js

--- travel-details/.next/static/chunks/webpack.js
+++ travel-details/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "065c04af50028b008e1c"; }
+/******/ 		__webpack_require__.h = function() { return "7bdd456ba54df5a63b7a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
