8027dbc65d89a7eb8573f33db08e1176884a3dde
Tyler Ma <tylermavan@gmail.com>
2021-08-20 04:46:05 +0000

    Install remark & date-fns packages

--- travel-details/pages/posts/[id].tsx
+++ travel-details/pages/posts/[id].tsx
@@ -1,12 +1,25 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+// Add this import
+import Head from 'next/head'
 
-export async function getStaticPaths() {
-  const paths = getAllPostIds()
-  return {
-    paths,
-    fallback: false
-  }
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {/* Add this <Head> tag */}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
+      {/* Keep the existing code here */}
+    </Layout>
+  )
+}
+
+export async function getStaticProps({ params }) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id)
+  // ...
 }
 
 export async function getStaticProps({ params }) {
@@ -26,6 +39,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
 }
